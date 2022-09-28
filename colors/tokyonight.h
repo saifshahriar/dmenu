2c23b17d512bd90844cba5663e0ad8deef0e3e25
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-09-28 04:42:11 +0000

    Updated Colors

--- colors/tokyonight.h
+++ colors/tokyonight.h
@@ -1,10 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 							/*     fg         bg       */
 	[SchemeNorm] 			= { "#c0c0f5", "#1a1b26" },
-	[SchemeSel]				= { "#c0caf5", "#414868" },
+	[SchemeSel]				= { "#1a1b26", "#7aa2f7" },
 	[SchemeSelHighlight] 	= { "#7aa2f7", "#33467c" },
 	[SchemeNormHighlight] 	= { "#7aa2f7", "#1c1f24" },
 	[SchemeOut] 			= { "#ffffff", "#458588" },
 	[SchemeMid] 			= { "#e0af68", "#1c1f24" },
 };
-
